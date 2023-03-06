a16f89af85085d9ccccf52f976baa2cc8d7f6548
[InaLuo2022] <117559158+InaLuo2022@users.noreply.github.com>
2023-03-07 00:00:40 +1100

    update

--- static/js/app.js
+++ static/js/app.js
@@ -5,11 +5,14 @@ url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/1
 d3.json(url).then(function(data){
     console.log(data.samples[0]);
     let data_sample = data.samples[0];
+    let data_metadata = data.metadata[0]
     console.log(data_sample);
+    console.log(data_metadata)
 });
 
 // Initialize the default plot with the first data sample
 d3.json(url).then(function init(first_sample) {
+    // bar chart
     let otu_ids = [];
     let data_sample = first_sample.samples[0]
 
@@ -30,12 +33,48 @@ d3.json(url).then(function init(first_sample) {
 
     var layout = {
         title: 'top 10 OTUs',
-        height: 600,
-        width: 800,
+        height: 400,
+        width: 600,
         rotation: 'h'
     };
 
-    Plotly.newPlot("plot", data, layout);
+    Plotly.newPlot("bar", data, layout);
+
+    // bubble chart
+    let bubble_size = []
+        for (let z = 0; z < 80; z++) {
+        bubble_size.push(data_sample.sample_values[z] * 0.8);}
+
+    var data_bubble = [{
+        x: data_sample.otu_ids,
+        y: data_sample.sample_values,
+        mode: 'markers',
+        marker: { 
+            size: bubble_size,
+            color: data_sample.otu_ids,
+        }
+    }];
+
+    var layout = {
+        title: 'otu_lable',
+        showlegend: false,
+        height: 400,
+        width: 800,
+      };
+
+    Plotly.newPlot('bubble', data_bubble, layout);
+
+    //sample-metadata
+    paragraphs = [];
+
+    paragraphs.push('<p>' + "id: " + first_sample.metadata[0].id + '</p>');
+    paragraphs.push('<p>' + "ethnicity: " + first_sample.metadata[0].ethnicity + '</p>');
+    paragraphs.push('<p>' + "gender: " + first_sample.metadata[0].gender + '</p>');
+    paragraphs.push('<p>' + "location: " + first_sample.metadata[0].location + '</p>');
+    paragraphs.push('<p>' + "bbtype: " + first_sample.metadata[0].bbtype + '</p>');
+    paragraphs.push('<p>' + "wfreq: " + first_sample.metadata[0].wfreq + '</p>')
+
+    document.getElementById("sample-metadata").innerHTML = paragraphs;
 });
 
 // set dropdown box value
@@ -82,12 +121,53 @@ function getData() {
 
         var layout = {
             title: 'top 10 OTUs',
-            height: 600,
-            width: 800,
+            height: 400,
+            width: 600,
             rotation: 'h'
         };
 
-        Plotly.newPlot("plot", data, layout);
+        Plotly.newPlot("bar", data, layout);});
+
+    d3.json(url).then(function(chart_2){
+
+        // let otu_lable = 'OTU' + data_sample.otu_lable[index];
+        let bubble_size = []
+        for (let z = 0; z < 153; z++) {
+            bubble_size.push(chart_2.samples[index].sample_values[z] * 0.8);}
+
+        var data_bubble = [{
+            x: chart_2.samples[index].otu_ids,
+            y: chart_2.samples[index].sample_values,
+            mode: 'markers',
+            marker: { 
+                size: bubble_size,
+                color: chart_2.samples[index].otu_ids,
+            }
+        }];
+
+        var layout = {
+            title: 'otu_lable',
+            showlegend: false,
+            height: 400,
+            width: 800,
+          };
+
+    Plotly.newPlot('bubble', data_bubble, layout);
+    });
+
+    d3.json(url).then(function(Demo_info){
+        paragraphs = [];
+
+        paragraphs.push('<p>' + "id: " + Demo_info.metadata[index].id + '</p>');
+        paragraphs.push('<p>' + "ethnicity: " + Demo_info.metadata[index].ethnicity + '</p>');
+        paragraphs.push('<p>' + "gender: " + Demo_info.metadata[index].gender + '</p>');
+        paragraphs.push('<p>' + "location: " + Demo_info.metadata[index].location + '</p>');
+        paragraphs.push('<p>' + "bbtype: " + Demo_info.metadata[index].bbtype + '</p>');
+        paragraphs.push('<p>' + "wfreq: " + Demo_info.metadata[index].wfreq + '</p>')
+
+        document.getElementById("sample-metadata").innerHTML = paragraphs;
+
     });
 };
+
 
